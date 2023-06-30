b93f19026c05062f1ce3d0b2b4505649b0b7388b
NewBean0312 <newbean0312@gmail.com>
2023-06-30 20:15:54 +0900

    기본 설정 및 슬라이드 배너 설정

---
+++ slide_banner.js
@@ -0,0 +1,46 @@
+var slides = document.querySelector(".slides"),
+  slide = document.querySelectorAll(".slides li"),
+  currentIdx = 0,
+  slideCount = slide.length,
+  slideWidth = 393.75,
+  slideMargin = 30,
+  prevBtn = document.querySelector(".prev"),
+  nextBtn = document.querySelector(".next");
+
+slides.style.width =
+  (slideWidth + slideMargin) * slideCount - slideMargin + "px";
+
+prevBtn.classList.add("none");
+
+function moveSlide(num) {
+  slides.style.left = -num * (slideWidth + slideMargin) + "px";
+  currentIdx = num;
+  updateBtn();
+}
+
+function updateBtn() {
+  console.log(currentIdx);
+  if (currentIdx == 0) {
+    prevBtn.classList.add("none");
+  } else {
+    prevBtn.classList.remove("none");
+  }
+
+  if (currentIdx == slideCount - 4) {
+    nextBtn.classList.add("none");
+  } else {
+    nextBtn.classList.remove("none");
+  }
+}
+
+prevBtn.addEventListener("click", function () {
+  if (currentIdx > 0) {
+    moveSlide(currentIdx - 1);
+  }
+});
+
+nextBtn.addEventListener("click", function () {
+  if (currentIdx < slideCount - 3) {
+    moveSlide(currentIdx + 1);
+  }
+});
