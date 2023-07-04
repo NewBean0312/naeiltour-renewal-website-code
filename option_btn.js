1c06efb5b54de22886f5bbe27916544f0ad99d34
NewBean0312 <newbean0312@gmail.com>
2023-07-04 13:05:32 +0900

    option_btn.js 랜덤 함수에 따른 무작위 배열 생성

---
+++ option_btn.js
@@ -0,0 +1,6 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const shuffledArray = array.sort(() => Math.random() - 0.5);
+
+console.log(array);
+console.log(shuffledArray);
+
--- slide_banner.js
+++ slide_banner.js
@@ -1,3 +1,4 @@
+// 변수명 지정
 var slides = document.querySelector(".slides"),
   slide = document.querySelectorAll(".slides li"),
   currentIdx = 0,
@@ -7,17 +8,22 @@ var slides = document.querySelector(".slides"),
   prevBtn = document.querySelector(".prev"),
   nextBtn = document.querySelector(".next");
 
+
+// 슬라이더 길이 지정
 slides.style.width =
   (slideWidth + slideMargin) * slideCount - slideMargin + (-5)+ "px";
 
+// 이전 버튼 display 제거
 prevBtn.classList.add("none");
 
+// 버튼 클릭 시, 슬라이더 위치 이동
 function moveSlide(num) {
   slides.style.left = -num * (slideWidth + slideMargin) + "px";
   currentIdx = num;
   updateBtn();
 }
 
+// 첫 or 마지막 슬라이더 시, 버튼 제거 및 생성
 function updateBtn() {
   console.log(currentIdx);
   if (currentIdx == 0) {
