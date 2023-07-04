ecc2bc5a8607dd437b0ca4d290a77649a05f4cd0
NewBean0312 <newbean0312@gmail.com>
2023-07-04 21:58:20 +0900

    slide_banner.js 병합 및 slides left 초기화 및 arrow display 초기화

--- slide_banner.js
+++ slide_banner.js
@@ -6,12 +6,16 @@ var slides = document.querySelector(".slides"),
   slideWidth = 396,
   slideMargin = 30,
   prevBtn = document.querySelector(".prev"),
-  nextBtn = document.querySelector(".next");
+  nextBtn = document.querySelector(".next"),
+  // 무작위 배열 변수 설정
+  searchBtn = document.querySelector(".search-btn"),
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+  shuffledArray = 0;
 
 
 // 슬라이더 길이 지정
 slides.style.width =
-  (slideWidth + slideMargin) * slideCount - slideMargin + (-5)+ "px";
+  (slideWidth + slideMargin) * slideCount - slideMargin + -5 + "px";
 
 // 이전 버튼 display 제거
 prevBtn.classList.add("none");
@@ -49,4 +53,21 @@ nextBtn.addEventListener("click", function () {
   if (currentIdx < slideCount - 8) {
     moveSlide(currentIdx + 1);
   }
+});
+
+// 무작위로 slide 순서 지정
+function shuffle() {
+  shuffledArray = [...array].sort(() => Math.random() - 0.5);
+  console.log(shuffledArray);
+}
+
+searchBtn.addEventListener("click", function () {
+  shuffle();
+  for (var i = 0; i < array.length; i++) {
+    slide[i].style.order = shuffledArray[i];
+  }
+  slides.style.left = 0;
+  currentIdx = 0;
+  prevBtn.classList.add("none");
+  nextBtn.classList.remove("none");
 });
