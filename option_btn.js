aab7881b168025aa34e3376fc7aaa04e92abfff4
NewBean0312 <newbean0312@gmail.com>
2023-07-04 21:46:34 +0900

    option_btn.js 랜덤 배열을 slide li의 order로 지정

--- option_btn.js
+++ option_btn.js
@@ -1,6 +1,18 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const shuffledArray = array.sort(() => Math.random() - 0.5);
 
+var slide = document.querySelectorAll(".slides li"),
+  searchBtn = document.querySelector(".search-btn"),
+  shuffledArray = 0;
+
+function shuffle() {
+  shuffledArray = [...array].sort(() => Math.random() - 0.5);
+  console.log(shuffledArray);
+}
 console.log(array);
-console.log(shuffledArray);
-
+// 무작위로 slide 순서 지정
+searchBtn.addEventListener("click", function () {
+  shuffle();
+  for (var i = 0; i < array.length; i++) {
+    slide[i].style.order = shuffledArray[i];
+  }
+});
